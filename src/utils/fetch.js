d62b489f4191431e43a28f114624eab31bee6e4e
Omri Levy <omri.lvy@gmail.com>
2024-05-09 11:02:55 +0300

    update fetch with defaults values

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,4 +1,5 @@
-const defaultServer = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_BACKEND_ADDRESS_DEV : process.env.REACT_APP_BACKEND_ADDRESS_PROD
+const env = process.env.NODE_ENV || 'development';
+const defaultServer = env === 'development' ? process.env.REACT_APP_BACKEND_ADDRESS_DEV || 'http://localhost:5000' : process.env.REACT_APP_BACKEND_ADDRESS_PROD
 export const fetcher = async (url, method = 'GET', body = null, headers = {}) => {
     console.log(`${defaultServer}/${url}`)
     try {
